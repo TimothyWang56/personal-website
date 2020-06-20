96fdbe703e702dc3b029b5d03f336f20011876e1
TimothyWang56 <timothywang56@gmail.com>
2020-06-20 19:43:12 -0400

    added github/devpost hyperlinks to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,6 +7,14 @@ class ProjectCard extends React.Component {
     return this.props.logo !== null ? <img src={this.props.logo} alt={""} className="front-image"/> : <div className="front-text">{this.props.frontText}</div>
   }
 
+  renderLinks() {
+    const links = [
+      this.props.githubLink !== null ? <a href={this.props.githubLink}>GITHUB</a> : null,
+      this.props.devpostLink !== null ? <a href={this.props.devpostLink}>DEVPOST</a> : null
+    ];
+    return links;
+  }
+
   render() {
     return (
       <div className="project-flip-card">
@@ -16,7 +24,9 @@ class ProjectCard extends React.Component {
           </div>
           <div className="project-flip-card-back">
             <h4>{this.props.projectName}</h4>
-            <p>{this.props.description}</p><p>{this.props.technologies}</p>
+            <p>{this.props.description}</p>
+            <p><b>Built using: </b>{this.props.technologies}</p>
+            <div className="links-container">{this.renderLinks()}</div>
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,46 +4,36 @@ import ProjectCard from './ProjectCard';
 
 const projectInfo = [
   {
-    projectName: "Give Your Two Cents",
-    logo: require("../assets/images/giveyourtwocentslogo1.png"),
-    description: "Chrome extension for Hack the Northeast 2020 and Winner of Most Viable Startup Category",
-    technologies: "JavaScript, HTML, CSS"
-  },
-  {
     projectName: "The Admiral",
     logo: require("../assets/images/theadmiral.gif"),
     description: "4-player computer game combining elements from social deduction and racing games",
-    technologies: "C#, Unity"
+    technologies: "C#, Unity",
+    githubLink: null,
+    devpostLink: null,
   },
   {
     projectName: "Rhyme Time",
     logo: require("../assets/images/rhymetime-model.png"),
     description: "LSTM neural network that generates limerick poems with perplexity of ~4.1",
-    technologies: "Python, Tensorflow"
+    technologies: "Python, Tensorflow",
+    githubLink: "https://github.com/zhaovan/lstm-rhyme-time",
+    devpostLink: null,
   },
   {
     projectName: "Rubik's Cube Simulator",
     logo: require("../assets/images/cube.gif"),
     description: "Simulator for a 3x3 Rubik's cube using keyboard inputs",
-    technologies: "Python, PyGame, OpenGL"
+    technologies: "Python, PyGame, OpenGL",
+    githubLink: "https://github.com/TimothyWang56/rubiks-cube",
+    devpostLink: null,
   },
   {
     projectName: "Give Your Two Cents",
     logo: require("../assets/images/giveyourtwocentslogo1.png"),
-    description: "Project for HTNE 2020",
-    technologies: "JavaScript, HTML, CSS, Chrome Extension"
-  },
-  {
-    projectName: "Give Your Two Cents",
-    logo: require("../assets/images/giveyourtwocentslogo1.png"),
-    description: "Project for HTNE 2020",
-    technologies: "JavaScript, HTML, CSS, Chrome Extension"
-  },
-  {
-    projectName: "Give Your Two Cents",
-    logo: require("../assets/images/giveyourtwocentslogo1.png"),
-    description: "Project for HTNE 2020",
-    technologies: "JavaScript, HTML, CSS, Chrome Extension"
+    description: "Chrome extension for Hack the Northeast 2020 and Winner of Most Viable Startup Category",
+    technologies: "JavaScript, HTML, CSS",
+    githubLink: "https://github.com/TimothyWang56/give-your-two-cents",
+    devpostLink: "https://devpost.com/software/charity-tracker-chrome-extension",
   },
 ]
 
@@ -59,7 +49,9 @@ class Projects extends React.Component {
           logo={project.logo}
           alt={project.alt}
           frontText={project.frontText}
-          technologies={"Built using: " + project.technologies}
+          technologies={project.technologies}
+          githubLink={project.githubLink}
+          devpostLink={project.devpostLink}
         />
       )
     });
