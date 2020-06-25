2391206480e851363bf43b4cdcd97982f4008d0c
TimothyWang56 <timothywang56@gmail.com>
2020-06-25 15:57:44 -0400

    Updates

---
+++ precache-manifest.3f9ce0ae648f9c5b49f9cad26f760530.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fecd6f6eff486016c20ca228da3ee9",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "006aefff3edd70065c74",
+    "url": "/personal-website/static/css/main.da2b53a0.chunk.css"
+  },
+  {
+    "revision": "8faaeb87a60fe27649ac",
+    "url": "/personal-website/static/js/2.80258b49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.80258b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006aefff3edd70065c74",
+    "url": "/personal-website/static/js/main.7e8958b4.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "/personal-website/static/media/JosefinSans-VariableFont_wght.324a513c.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/personal-website/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "7cfbd4284ec01b7ace2f8edb5cddae84",
+    "url": "/personal-website/static/media/RobotoMono-Medium.7cfbd428.ttf"
+  },
+  {
+    "revision": "b691e48c4edf22ebc74fcfd178baa040",
+    "url": "/personal-website/static/media/azure-app-service.b691e48c.png"
+  },
+  {
+    "revision": "d37398a27bf9071eec4689e032c3ecb5",
+    "url": "/personal-website/static/media/azure-cosmos-db.d37398a2.png"
+  },
+  {
+    "revision": "c4c8c6410e0a5c9adfbf9ec584f8751c",
+    "url": "/personal-website/static/media/cube.c4c8c641.gif"
+  },
+  {
+    "revision": "e508ad042fd194bbc180b1418c0337c4",
+    "url": "/personal-website/static/media/giveyourtwocentslogo1.e508ad04.png"
+  },
+  {
+    "revision": "b14f2b8c1e8dc6f72d12d5737a6b4b67",
+    "url": "/personal-website/static/media/ocaml.b14f2b8c.png"
+  },
+  {
+    "revision": "9d53f3e505e16ed8bbc26ab2d7757fb6",
+    "url": "/personal-website/static/media/portrait.9d53f3e5.png"
+  },
+  {
+    "revision": "5274ce48c23ba305e1c3ce55eb5b73c0",
+    "url": "/personal-website/static/media/racket-logo.5274ce48.png"
+  },
+  {
+    "revision": "d00862e08e577fa7d60dcb2dfd63d664",
+    "url": "/personal-website/static/media/rhymetime-model.d00862e0.png"
+  },
+  {
+    "revision": "ce041b7f26143aac53bf51594cd59a5c",
+    "url": "/personal-website/static/media/theadmiral.ce041b7f.gif"
+  }
+]);
