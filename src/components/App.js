75a441684ec516297a0fabe6c06fc7ca3cf3f554
TimothyWang56 <timothywang56@gmail.com>
2020-06-28 22:21:42 -0400

    added simple footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Intro from './Intro';
 import Projects from './Projects';
 import Experience from './Experience';
 import Skills from './Skills';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Experience/>
         <Projects/>
         <Skills/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Footer.css';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className='footer'>
+        <p>Created by Timothy Wang @2020</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
