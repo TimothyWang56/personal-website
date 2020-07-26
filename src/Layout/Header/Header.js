646440a11adb9a33a8de5414c7278ff75871ee8e
TimothyWang56 <timothywang56@gmail.com>
2020-07-25 22:51:40 -0400

    display date on header

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -2,36 +2,77 @@ import React from 'react';
 import './Header.scss';
 
 import NavButton from '../../Components/NavButton/NavButton';
+import { MdMenu } from 'react-icons/md';
 
 const pages = [
     {name: "Home", navigation: "/home"},
     {name: "Experiences", navigation: "/experiences"},
-    {name: "Projects", navigation: "/projects"},
     {name: "Skills", navigation: "/skills"},
+    {name: "Projects", navigation: "/projects"},
+    
 ]
+
+const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const shortDayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const shortMonthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 class Header extends React.Component {
 
-    renderNavButtons() {
-        return pages.map((page) =>
-            <div className="nav-button">
-                <NavButton
-                    navigation={page.navigation}
-                    buttonText={page.name} 
-                    active={this.props.active === page.name}/>
-            </div>
+    renderNavButtons(startIndex, endIndex) {
+        if (!endIndex) {
+            endIndex = startIndex;
+        }
+
+        return pages.map((page, index) =>
+            index >= startIndex && index <= endIndex ?
+                <div className='nav-button'>
+                    <NavButton
+                        navigation={page.navigation}
+                        buttonText={page.name.toUpperCase()} 
+                        active={this.props.active === page.name}/>
+                </div>
+            :
+                null
         );
     }
 
-    render() {        
+    render() {
+        const d = new Date();
+        const dayOfWeek = this.props.shortened ? shortDayName[d.getDay()] : dayName[d.getDay()];
+        const date = d.getDate();
+        const month = this.props.shortened ? shortMonthName[d.getMonth()] : monthName[d.getMonth()];
+        const year = d.getFullYear();
+        const formattedDate = dayOfWeek.toUpperCase() + ', ' + month.toUpperCase() + ' ' + date + ', ' + year;
+
         return (
             <div className='header-wrapper'>
                 <div className='header-text'>
                     {this.props.headerText}
                 </div>
                 <hr className='horizontal-black-line'/>
-                <div className="nav-buttons-wrapper">
-                    {this.renderNavButtons()}
+                <div className='bar-wrapper'>
+                {
+                    !this.props.shortened ? 
+                        [
+                            <div className='left-nav-buttons'>
+                                {this.renderNavButtons(0, 1)}
+                            </div>,
+                            <div className='date-wrapper'>
+                                {formattedDate}
+                            </div>,
+                            <div className='right-nav-buttons'>
+                                {this.renderNavButtons(2, 3)}
+                            </div>,
+                        ]
+                    :
+                        [
+                            <div className='date-wrapper'>
+                                {formattedDate}
+                            </div>,
+                        ]
+                }
                 </div>
                 <hr className='horizontal-black-line'/>
             </div>
--- src/Layout/Page/Page.js
+++ src/Layout/Page/Page.js
@@ -4,6 +4,34 @@ import './Page.scss';
 import Header from '../../Layout/Header/Header';
 
 class Page extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            shortenedHeader: false,
+        }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+		this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	updateWindowDimensions() {
+        let shortenedHeader = false;
+
+        if (window.innerWidth < window.innerHeight) {
+            shortenedHeader = true;
+        }
+
+        this.setState({ shortenedHeader });
+	}
 
     render() {        
         return (
@@ -11,6 +39,7 @@ class Page extends React.Component {
                 <Header
                     headerText='TimothyWang'
                     active={this.props.active}
+                    shortened={this.state.shortenedHeader}
                 />
                 <div className='page-content-wrapper'>
                     {this.props.children}
