0ce11b9ce7d98775a501f7742cd99d73994d846c
TimothyWang56 <timothywang56@gmail.com>
2020-07-25 20:14:39 -0400

    change font size of article card and formatting with no picture

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './HomePage.scss';
 
 import Page from '../../Layout/Page/Page';
+import ArticleCard from '../../Components/ArticleCard/ArticleCard';
 
 class HomePage extends React.Component {
     render() {
         return (
             <div>
                 <Page active='Home'>
-                    Home
+                    <div className='about-me-card'>
+                        <ArticleCard image={require('../../Assets/Images/portrait.png')}/>
+                    </div>
                 </Page>
             </div>
         );
