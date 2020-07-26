c44f893567723cda8183430baa1693700d228d87
TimothyWang56 <timothywang56@gmail.com>
2020-07-25 22:01:22 -0400

    finish article card and about me on home page

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -3,22 +3,34 @@ import './ArticleCard.scss';
 
 class ArticleCard extends React.Component {
     render() {
+        const style = this.props.horizontalOrientation ?
+            { 'gridTemplateColumns': '1fr 3fr' } :
+            { 'gridTemplateRows': 'auto' }
+
+        const portraitStyle = this.props.horizontalOrientation ?
+            { 'width': '25vw', 'height': 'auto'} :
+            { 'width': '70%', 'height': 'auto', 'paddingBottom': '2em' }
+
+        const textStyle = this.props.horizontalOrientation ?
+            { 'paddingLeft': '1em', 'fontSize': '1.4vw' } :
+            { 'paddingLeft': '0em', 'fontSize': '1em' }
+
         return (
-            <div className='article-card-wrapper'>
+            <div className='article-card-wrapper' style={style}>
                 {this.props.image &&
-                <div className='picture-wrapper'>
-                    <img
-                        src={this.props.image}
-                        alt='portrait'
-                        className='picture'
-                    />
-                </div>}
-                <div className={'article-wrapper' + (this.props.image ? '' : ' no-image')}>
+                <img
+                    src={this.props.image}
+                    alt='portrait'
+                    className='picture'
+                    style={portraitStyle}
+                />}
+                <div
+                    className={'article-wrapper' + (this.props.image ? '' : ' no-image')}
+                    style={textStyle}
+                >
                     <h1 className='article-title'>About Me</h1>
                     <hr className='horizontal-card-black-line'/>
-                    <div className='article-text'>
-                    Hey there, and welcome to my website! I'm a rising junior at Brown University studying Applied Mathematics and Computer Science. In my free time, I really enjoy watching shows, playing board games, and solving puzzles. Since coming home during the pandemic, my friends and I have been playing a lot of online Catan, so definitely hit me up if you love to play as well! One of my biggest hobbies growing up was speed-solving Rubik's cubes. I used to attend competitions as well, and while I'm not quite as addicted as I used to be, I still keep my cubes near my desk for whenever I need a break from what I'm doing. More recently, I've been getting really into crossword puzzles, and I have been solving the NYT crossword everyday since February with a group of friends. A really cool variant of crossword puzzles that I recently discovered is cryptic crosswords. Here's an example of a cryptic clue: "really large bird I can stew" (7). The answer is titanic. Here's why: titanic means "really large", a tit is a "bird", and anic is "I can" stewed - in other words, the letters in "I can" mixed up. Really neat right?
-                    </div>
+                    {this.props.children}
                 </div>
             </div>
         );
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,19 +1,88 @@
 import React from 'react';
 import './HomePage.scss';
 
+import { MdDescription } from "react-icons/md";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+
 import Page from '../../Layout/Page/Page';
 import ArticleCard from '../../Components/ArticleCard/ArticleCard';
 
+const aboutMeText = "Hey there, and welcome to my website! " + 
+    "I'm a rising junior at Brown University studying Applied Mathematics and Computer Science. " +
+    "In my free time, I really enjoy watching shows, playing board games, and solving puzzles. " +
+    "Since coming home during the pandemic, my friends and I have been playing a lot of online Catan, " +
+    "so definitely hit me up if you love to play as well! One of my biggest hobbies growing up was " +
+    "speed-solving Rubik's cubes. I used to attend competitions as well, and while I'm not quite as " +
+    "addicted as I used to be, I still keep my cubes near my desk for whenever I need a break from what " +
+    "I'm doing. More recently, I've been getting really into crossword puzzles, and I have been solving " +
+    "the NYT crossword everyday since February with a group of friends. A really cool variant of crossword " +
+    "puzzles that I recently discovered is cryptic crosswords. Here's an example of a cryptic clue: \"really " +
+    "large bird I can stew\" (7). The answer is titanic. Here's why: titanic means \"really large\", a tit is a " +
+    "\"bird\", and anic is \"I can\" stewed - in other words, the letters in \"I can\" mixed up. Really neat right?"
+
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            horizontalOrientation: true,
+        }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+		this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	updateWindowDimensions() {
+        let horizontalOrientation = true;
+
+        if (window.innerWidth < window.innerHeight) {
+            horizontalOrientation = false;
+        }
+
+        this.setState({ horizontalOrientation });
+	}
+
     render() {
+        const cardStyle = this.state.horizontalOrientation ?
+            { 'width': '80%' } :
+            { 'width': '95%' }
+
         return (
-            <div>
-                <Page active='Home'>
-                    <div className='about-me-card'>
-                        <ArticleCard image={require('../../Assets/Images/portrait.png')}/>
+            <Page active='Home'>
+                <div className='home-wrapper'>
+                    <div style={cardStyle}>
+                        <ArticleCard
+                            image={require('../../Assets/Images/portrait.png')}
+                            horizontalOrientation={this.state.horizontalOrientation}
+                        >
+                            {aboutMeText}
+                            <div className='icon-bar'>
+                                <a href='' target='_blank' className='icon'>
+                                    <MdDescription size='2.5em' title='resume'/>
+                                </a>
+                                <a href='mailto:timothywang56@gmail.com' target='_blank' className='icon'>
+                                    <FaEnvelope size='2.5em' title='email'/>
+                                </a>
+                                <a href='https://linkedin.com/in/timothywang56' target='_blank' className='icon'>
+                                    <FaLinkedin size='2.5em' title='linkedin'/>
+                                </a>
+                                <a href='https://github.com/TimothyWang56' target='_blank' className='icon'>
+                                    <FaGithub size='2.5em' title='github'/>
+                                </a>
+                            </div>
+                        </ArticleCard>
+
                     </div>
-                </Page>
-            </div>
+                </div>
+            </Page>
         );
     }
 }
