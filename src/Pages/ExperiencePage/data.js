9ac241bc6f6757a46510330182d9534d9af4bb73
TimothyWang56 <timothywang56@gmail.com>
2020-08-12 14:54:52 -0400

    update experiences

--- src/Pages/ExperiencePage/data.js
+++ src/Pages/ExperiencePage/data.js
@@ -12,11 +12,12 @@ export const crossword = [
 ]
 
 const cressText =
-    "I'm a Software Engineering Intern for Cress Health, a startup that's aiming to help workers in the healthcare industry manage their mental wellness. " +
+    "I was a Software Engineering Intern for Cress Health, a startup that's aiming to help workers in the healthcare industry manage their mental wellness. " +
     "I started working for Cress at the beginning of my Sophomore Spring semester as a Front-End Developer for Cress's mobile application, but for the summer, " +
-    "I've taken on new responsibilities. For the first month of the summer, I worked with 1 other intern on a dashboard tool that congregates data from the mobile app " +
-    "and displays the anonymized data in customizable graphs. It was built using React.js, Express, and Azure Cosmos DB. I am now currently leading a team of 5 other interns " +
-    "in building an internal Feature Flag tool to aid our developers with feature development and rollout."
+    "I took on new responsibilities. For the first month of the summer, I worked with 1 other intern on a dashboard tool that congregates data from the mobile app " +
+    "and displays the anonymized data in customizable graphs. It was built using React.js, Express, and Azure Cosmos DB. For the second half of the summer, I led a team of 5 other interns " +
+    "in building an internal Feature Flag tool to aid our developers with feature development and rollout. " +
+    "This included designing the application from scratch, scheduling regular meetings with the other interns, and distributing the tasks."
 
 const htaText =
     "I'm a Head Teaching Assistant for one of Brown University's introductory computer science courses, CSCI0170 - Computer Science: An Integrated Introduction. " +
@@ -40,7 +41,7 @@ export const clues = [
     {
         num: 1,
         row: true,
-        dates: 'Jan 2020 - Present',
+        dates: 'Jan 2020 - Aug 2020',
         title: 'Full-Stack Software Engineering Intern',
         text: cressText,
     },
